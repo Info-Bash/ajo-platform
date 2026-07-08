c53f1b4991759ff6abfa194835c6eaeb30da9126
Bash <abubakarbabatunde20@gmail.com>
2026-07-09 00:54:06 +0100

    feat(contributions): add ChatModule to contributions module imports

--- ajo-server/src/contributions/contributions.module.ts
+++ ajo-server/src/contributions/contributions.module.ts
@@ -3,9 +3,10 @@ import { ContributionsService } from './contributions.service';
 import { ContributionsController } from './contributions.controller';
 import { ContributionsCronService } from './contributions-cron.service';
 import { PayoutsModule } from '../payouts/payouts.module';
+import { ChatModule } from '../chat/chat.module';
 
 @Module({
-  imports: [PayoutsModule],
+  imports: [PayoutsModule, ChatModule],
   controllers: [ContributionsController],
   providers: [ContributionsService, ContributionsCronService],
   exports: [ContributionsService],
